cfa2e229d4f26a004e899e4e3d31c756177727da
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-08-17 20:17:06 -0400

    mudanças de n2

--- mapper.py
+++ mapper.py
@@ -11,12 +11,12 @@ class Mapper:
   def Map(self):
     for line in sys.stdin:
       line = line.strip()
-      line = re.sub(r'[^\w\s]', '',line)
+      # line = re.sub(r'[^\w\s]', '',line)
       line = line.lower()
       for x in line:
         if x in punctuations:
           line = line.replace(x, " ")
-      words = line.split('\t')
+      words = line.split()
       for word in words:
         self.H[word] += 1
       
