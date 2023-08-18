83c38779e19430e22d2c9a91e86c3f8d188eb195
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-08-17 21:27:49 -0400

    reduceer

--- reducer.py
+++ reducer.py
@@ -7,10 +7,9 @@ word = None
 total_unique = 0
 palavras_frequentes = {}
 # input comes from STDIN
-for line in sys.stdin:
+for line in io.TextIOWrapper(sys.stdin.buffer, encoding='latin1'):
     # remove leading and trailing whitespace
     line = line.strip()
-    line=line.lower()
 
     # parse the input we got from mapper.py
     word, count = line.split('\t', 1)
@@ -22,44 +21,45 @@ for line in sys.stdin:
       continue
 
 
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # coloca no dic as palavras e as frequencias que aparecem
-            if current_word in palavras_frequentes:
-                palavras_frequentes[current_word] += current_count
-            else:
-                palavras_frequentes[current_word] = current_count
+    # # this IF-switch only works because Hadoop sorts map output
+    # # by key (here: word) before it is passed to the reducer
+    # if current_word == word:
+    #     current_count += count
+    # else:
+    #     if current_word:
+    #         # coloca no dic as palavras e as frequencias que aparecem
+    #         if current_word in palavras_frequentes:
+    #             palavras_frequentes[current_word] += current_count
+    #         else:
+    #             palavras_frequentes[current_word] = current_count
             
-            # write result to STDOUT
-            print ('%s\t%s' % (current_word, current_count))
-            # Contar palavras que nao repetem
-            # total_unique += 1
-        current_count = count
-        current_word = word
+    #         # write result to STDOUT
+    #         print ('%s\t%s' % (current_word, current_count))
+    #         # Contar palavras que nao repetem
+    #         # total_unique += 1
+    #     current_count = count
+    #     current_word = word
 
-# do not forget to output the last word if needed!
-if current_word == word:
-    print( '%s\t%s' % (current_word, current_count))
-       
-if current_word != word:
-    if current_word is not None and current_count == 1:
+    if current_count > 1:
         total_unique += 1
+    
 
-
-# print("============================ Histograma da colecao ============================")
-# for word, count in palavras_frequentes.items():
+print("============================ Histograma da colecao ============================")
+# for word, count in line.items():
 #     print(f"{word}:{count}")
+palavras_frequentes = dict(sorted(line.items(), key=lambda item:item[1], reverse=True))
+limite = 0
+for word in palavras_frequentes:
+    print(word)
+    limite += 1
+    if limite >= 10: break
 
-# print("============================ Top 10 ============================")
-# palavras_frequentes = dict(sorted(palavras_frequentes.items(), key=lambda item:item[1], reverse=True))
-# limite = 0
-# for word, count in palavras_frequentes.items():
-#     print(f"{word}:{count}")
-#     limite += 1
-#     if limite >= 10: break
+print("============================ Top 10 ============================")
+palavras_frequentes = dict(sorted(line.items(), key=lambda item:item[1], reverse=True))
+limite = 0
+for word, count in palavras_frequentes.items():
+    print(f"{word}:{count}")
+    limite += 1
+    if limite >= 10: break
 
-# print("Total unique words: %s" %(total_unique))
+print("Total unique words: %s" %(total_unique))
